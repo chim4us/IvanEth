7625ac93cc7bf1b8e809eec9b4aed9b7c113e6d5
Frank Chimdi <33489950+chim4us@users.noreply.github.com>
2021-06-14 20:54:12 +0100

    Update VotingSystem.sol
    Updated coded

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -22,31 +22,93 @@ contract Ballot{
     string public proposal;
     
     mapping(uint => vote) private votes;
-    mapping(address => voter) public voterRegistered;
+    mapping(address => voter) public voterRegister;
     
     enum State{Created, Voting, Ended}
     State public state;
     
     //modifiers
+    modifier condition(bool _condition){
+        require(_condition);
+        _;
+    }
+    
+    modifier onlyofficial(){
+        require(msg.sender ==ballotOfficailAddress);
+        _;
+    }
+    
+    modifier instate(State _state){
+        require(state == _state);
+        _;
+    }
     
     //events
     
     //functions
-    constructor(){
+    constructor(
+        string memory _ballotOfficialName,
+        string memory _proposal
+    ){
+        ballotOfficailAddress = msg.sender;
+        ballotOfficialName = _ballotOfficialName;
+        proposal = _proposal;
+        
+        state = State.Created;
+        totalVoter++;
+    }
+    
+    function addVoter(address _voteraddress,string memory _votersname)
+    public
+    instate(State.Created)
+    onlyofficial
+    {
+        voter memory v;
+        v.voterName = _votersname;
+        v.voted = false;
+        
+        voterRegister[_voteraddress] = v;
         
     }
     
-    function addVoter()public{
+    function startVote()
+    public
+    instate(State.Created)
+    onlyofficial
+    {
+        state = State.Voting;
+    }
+    
+    function doVote(bool _choice)
+    public
+    instate(State.Voting)
+    returns(bool voted)
+    {
+        bool found = false;
+        
+        if (bytes(voterRegister[msg.sender].voterName).length != 0 
+        && !voterRegister[msg.sender].voted){
+            voterRegister[msg.sender].voted = true;
+            vote memory v;
+            v.voterAddress = msg.sender;
+            v.choice = _choice;
+            if(_choice){
+                countResult ++;
+            }
+            votes[totalVote] = v;
+            totalVote++;
+            found = true;
+        }
+        return found;
         
     }
     
-    function startVote()public{
-        
-    }
-    function doVote()public{
-        
-    }
-    function endVote()public{
-        
+    function endVote()
+    public
+    instate(State.Voting)
+    onlyofficial
+    {
+        state = State.Ended;
+        finalResult = countResult;
     }
 }
