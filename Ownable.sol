aa907fdd236ca758a654f6e03cd1a78c7a752f48
Frank Chimdi <33489950+chim4us@users.noreply.github.com>
2021-06-17 12:23:43 +0100

    Update Ownable.sol
    Added pause to ownable

--- Ownable.sol
+++ Ownable.sol
@@ -1,14 +1,35 @@
 pragma solidity 0.5.12;
 
 contract ownable{
-    address internal owner; //internal make variables possible to be assess on external contract but its private on remix or public
+    address payable internal owner; //internal make variables possible to be assess on external contract but its private on remix or public
+    //address payable public owner;
+    bool private _paused;
     
     modifier onlyOwner (){
-        require(msg.sender == owner,"Only owner can delete");
+        require(msg.sender == owner,"Only owner have access to this");
         _;
+    }
+    
+    modifier whenNotPause(){
+        require(!_paused);
+        _;
+    }
+    
+    modifier whenPause(){
+        require(_paused);
+        _;
+    }
+    
+    function pause()public onlyOwner whenNotPause{
+        _paused = true;
+    }
+    
+    function unpause()public onlyOwner whenPause{
+        _paused = false;
     }
     
     constructor() public{
         owner = msg.sender;
+        _paused = false;
     }
 }
